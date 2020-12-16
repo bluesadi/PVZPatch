21e2367107cc99e4e387dfc47f4d68839758f572
bluesadi <liuyibo@stu.scu.edu.cn>
2020-12-16 16:06:49 +0800

    新增了一些功能

--- Injector.hpp
+++ Injector.hpp
@@ -5,6 +5,29 @@ void SetSunPickUp(DWORD sunPickUp) {
 	WriteDword(SUN_PICK_UP_ADDRESS, sunPickUp);
 }
 
+void SetGamePause(BOOL gamePause) {
+	if (gamePause == FALSE) {
+		WriteByte(JMP_GAME_PAUSE_ADDRESS, 0xEB);
+	}else {
+		WriteByte(JMP_GAME_PAUSE_ADDRESS, 0x74);
+	}
+}
+
+void SetMusicPause(BOOL musicPause) {
+	if (musicPause == FALSE) {
+		WriteByte(JMP_MUSIC_PAUSE_ADDRESS, 0xEB);
+	}else {
+		WriteByte(JMP_MUSIC_PAUSE_ADDRESS, 0x74);
+	}
+}
+
+void SetNoCooldown() {
+	WriteByte(SET_COOLDOWN_ADDRESS, 0x01);
+}
+
 void patch() {
 	SetSunPickUp(100); // 拾取一个阳光，总的阳光数增加100
+	SetGamePause(FALSE); // 鼠标点击窗口外时游戏不会暂停
+	SetMusicPause(FALSE); // 鼠标点击窗口外时音乐不会暂停
+	SetNoCooldown(); // 无冷却
 }
